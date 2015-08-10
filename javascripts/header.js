15437e2c6ed2b8d18b113a854db210fc804fda77
Evans Hall <msuech87@gmail.com>
2015-08-09 21:39:37 -0500

    Attempting to add a header

---
+++ javascripts/header.js
@@ -0,0 +1,8 @@
+document.write('<section class="page-header">');
+document.write('  <h1 class="project-name">The Lively Librarian!</h1>');
+document.write('  <h2 class="project-tagline"></h2>');
+document.write('  <a href="/about.html" class="btn">About</a>');
+document.write('  <a href="/blog.html" class="btn">Blog</a>');
+document.write('  <a href="/videos.html" class="btn">Videos</a>');
+document.write('  <a href="/pictures.html" class="btn">Pictures</a>');
+document.write('</section>');
